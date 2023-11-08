4ee199c9da6285ee0bde29c39af03a5977692e8d
1910220 <1910220@01-19852.pusd.dom>
2023-11-08 12:14:22 -0800

    Initial Commit

---
+++ File Reading/src/Runner.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Runner {
+
+	public static void main(String[] args)
+	{
+		File myFile = new File("BabyNames");
+		// use the scanner class
+		double total= 0;
+		
+		try {
+			Scanner myScanner = new Scanner(myFile);
+			
+			while(myScanner.hasNext()) {
+				 // int pos = myScanner.nextInt();
+				String girl = myScanner.next();
+				String boy = myScanner.next();
+				// What is the after age length of the girls names
+				total += girl.length();
+				System.out.println(myScanner.next());
+			}
+			total /= 100.0;
+			System.out.println(total + " Is the average length");
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// TODO Auto-generated method stub
+		
+		System.out.println("hellow World");
+		
+	}
+
+}
